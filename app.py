6b904891e40d92e44875a33b43a4a372bfd78e8d
nizarcan <nizarcan@yahoo.com>
2021-11-25 22:07:25 +0300

    updated app.py
    updated app.py to be fed with a post request

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from models.vrp import VRP
 
 
 app = Flask(__name__)
-
 HOST = "0.0.0.0"
 PORT = 5000
 
-@app.route("/")
+@app.route("/", methods=["POST"])
 def return_solution():
-    return jsonify({"message": "solution will be here"})
+    model = VRP(request.json)
+    model.build_model()
+    model.solve()
+    return jsonify(model.return_solution())
 
 if __name__ == '__main__':
     app.run(host=HOST, port=PORT)
