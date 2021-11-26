1569251b696bc691422832b86c4e0a5231cd3823
nizarcan <nizarcan@yahoo.com>
2021-11-26 20:34:23 +0300

    vrp class restructuring

--- models/vrp.py
+++ models/vrp.py
@@ -4,7 +4,10 @@ from ortools.constraint_solver import pywrapcp
 
 class VRP:
     def __init__(self):
-        # Initiating all attributes upon creating an instance
+        """
+        Initiating all attributes upon creating an instance
+        """
+        
         # Raw input data #
         self.vehicles = None
         self.jobs = None
@@ -28,43 +31,84 @@ class VRP:
         self.routing_model = None
 
     def load_data(self, input_dict):
-        # Destructure the data into class attributes
+        """
+        In this method, payload gets destructured into 
+        class elements for easier data access.
+        """
+        
         self.vehicles = input_dict["vehicles"]
         self.jobs = input_dict["jobs"]
         self.time_matrix = input_dict["matrix"]
 
     def generate_model_data(self):
-        location_count = len(self.time_matrix)
-        self.num_vehicles = len(self.vehicles)
-        self.time_matrix = [distance_row + [0] for distance_row in self.time_matrix]
-        self.time_matrix += [(location_count + 1) * [0]]
-        self.vehicle_capacities = [vehicle["capacity"][0] for vehicle in self.vehicles]
-        self.start_index = [vehicle["start_index"] for vehicle in self.vehicles]
-        self.end_index = self.num_vehicles * [location_count]
+        """
+        In this method, the raw data goes through
+        the data prep to be fed to the model.
 
-        self.demands = (location_count + 1) * [0]
-        self.service_time = len(self.time_matrix) * [0]
+        Things to look out: 
+        > This is a location-based model. In data prep phase, demands for same locations are pooled.
+        > Since this is a location-based model, job-location pairs are kept for desired output format.
+        > A return to starting point is not necessary for vehicles. Thus, a dummy node is created, its time
+            to other nodes are set to 0 and set as the ending node for each vehicle. 
+        """
+
+        # `num_locations` counts the dummy node too.
+        self.num_locations = len(self.time_matrix) + 1
+        self.num_vehicles = len(self.vehicles)
+
+        # Dummy node creation on the time matrix
+        self.time_matrix = [distance_row + [0] for distance_row in self.time_matrix]
+        self.time_matrix += [(self.num_locations) * [0]]
+
+        # Vehicle capacities extracted
+        self.vehicle_capacities = [vehicle["capacity"][0] for vehicle in self.vehicles]
+
+        # Starting index are extracted from `self.vehicles` and `end_index` is set as the dummy node
+        self.start_index = [vehicle["start_index"] for vehicle in self.vehicles]
+        self.end_index = self.num_vehicles * [self.num_locations - 1]
+
+        # Demands and service times are pooled w.r.t. location.
+        # Upon pooling, location-job info is kept in a dictionary.
+        self.demands = (self.num_locations) * [0]
+        self.service_time = (self.num_locations) * [0]
         self.location_job_pair = {}
         for job in self.jobs:
-            self.demands[job["location_index"]] += job["delivery"][0]
-            self.service_time[job["location_index"]] += job["service"]
-            if job["location_index"] in self.location_job_pair.keys():
-                self.location_job_pair[job["location_index"]].append(job["id"])
+            location_index = job["location_index"]
+            self.demands[location_index] += job["delivery"][0]
+            self.service_time[location_index] += job["service"]
+            if location_index in self.location_job_pair.keys():
+                self.location_job_pair[location_index].append(job["id"])
             else:
-                self.location_job_pair[job["location_index"]] = [job["id"]]
+                self.location_job_pair[location_index] = [job["id"]]
 
     def time_callback(self, from_index, to_index):
+        """
+        Time callback for objective function.
+        In the objective function that is going to be minimized,
+        service times are included. 
+        """
+
         from_node = self.routing_manager.IndexToNode(from_index)
         to_node = self.routing_manager.IndexToNode(to_index)
         return self.time_matrix[from_node][to_node] + self.service_time[to_node]
 
     def demand_callback(self, from_index):
+        """
+        The demand callback is for the only constraint to the model.
+        """
+
         from_node = self.routing_manager.IndexToNode(from_index)
         return self.demands[from_node]
 
     def build_model(self):
-        self.routing_manager = pywrapcp.RoutingIndexManager(len(self.time_matrix),
-                                                            len(self.vehicles),
+        """
+        The model object and route indexer is set 
+        as an attribute in the object to be 
+        later used in solution returning phase.         
+        """
+
+        self.routing_manager = pywrapcp.RoutingIndexManager(self.num_locations,
+                                                            self.num_vehicles,
                                                             self.start_index,
                                                             self.end_index
                                                             )
@@ -73,18 +117,28 @@ class VRP:
 
         transit_callback_index = self.routing_model.RegisterTransitCallback(self.time_callback)
 
+        # Objective function of the model
         self.routing_model.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
 
         demand_callback_index = self.routing_model.RegisterUnaryTransitCallback(self.demand_callback)
 
+        # Adding the demand constraint to the model
         self.routing_model.AddDimensionWithVehicleCapacity(
-            demand_callback_index,
-            0,
-            self.vehicle_capacities,
-            True,
-            "Capacity")
+            demand_callback_index,      # evaluator_index
+            0,                          # max_slack, which is 0 for our case
+            self.vehicle_capacities,    # vehicle capacity list
+            True,                       # the cumulative value of met demand starts from 0
+            "Capacity"                  # constraint name
+            )
 
     def solve(self):
+        """
+        For solution strategy, `PATH_CHEAPEST_ARC` for first solution and 
+        `GUIDED_LOCAL_SEARCH` for local search are used. For more, see; 
+            https://developers.google.com/optimization/routing/routing_options?hl=en#first_sol_options
+            https://developers.google.com/optimization/routing/routing_options?hl=en#local_search_options
+        """
+
         search_parameters = pywrapcp.DefaultRoutingSearchParameters()
         search_parameters.first_solution_strategy = (routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
         search_parameters.local_search_metaheuristic = (routing_enums_pb2.LocalSearchMetaheuristic.GUIDED_LOCAL_SEARCH)
@@ -93,43 +147,54 @@ class VRP:
         self.solution = self.routing_model.SolveWithParameters(search_parameters)
 
     def return_solution(self):
-        if self.solution:
-            solution_dict = {
-                "total_delivery_duration": 0,
-                "routes": {
-                    vehicle["id"]: {
-                    "jobs": [],
-                    "delivery_duration": 0
-                    } 
-                for vehicle in self.vehicles}
-                }
-            solution_dict["total_delivery_duration"] = 0
-            solution_dict["routes"] = {vehicle["id"]: {
-                "jobs": [],
-                "delivery_duration": 0
-            } for vehicle in self.vehicles}
+        """
+        Method that returns the solution at the structure stated in README.
+        """
 
+        if self.solution:
+            
+            total_delivery_duration = 0
+            routes = {}
+            # Looping over every vehicle
             for vehicle_idx in range(self.num_vehicles):
+                vehicle_jobs = []
+                vehicle_delivery_duration = 0
+
                 current_node = self.routing_model.Start(vehicle_idx)
                 vehicle_id = self.vehicles[vehicle_idx]["id"]
+
+                # Looping over every node in route of solution for selected vehicle
                 while not self.routing_model.IsEnd(current_node):
                     next_node = self.solution.Value(self.routing_model.NextVar(current_node))
                     node_idx = self.routing_manager.IndexToNode(current_node)
+                    
+                    # If there is a job for specified location, then the jobs are added to the solution dictionary. 
                     if node_idx in self.location_job_pair.keys():
                         for job_id in self.location_job_pair[node_idx]:
-                            solution_dict["routes"][vehicle_id]["jobs"].append(job_id)
-                    solution_dict["routes"][vehicle_id]["delivery_duration"] += self.routing_model.GetArcCostForVehicle(current_node, next_node, vehicle_idx)
+                            vehicle_jobs.append(job_id)
+                    vehicle_delivery_duration += self.routing_model.GetArcCostForVehicle(current_node, next_node, vehicle_idx)
                     current_node = next_node;
-                solution_dict["total_delivery_duration"] += solution_dict["routes"][vehicle_id]["delivery_duration"]
+
+                total_delivery_duration += vehicle_delivery_duration
+                routes[vehicle_id] = {
+                    "jobs": vehicle_jobs,
+                    "delivery_duration": vehicle_delivery_duration
+                }
+
+            solution_dict = {
+                "total_delivery_duration": total_delivery_duration,
+                "routes": routes
+            }
             
             return solution_dict
 
     def flush_model(self):
         '''
         This method is added to fix a memory leak.
-        Prior to usage of this method, routing_model attribute prevented
-        the garbage collector from collecting the whole object.
+        Before addition of this method, `routing_model` attribute 
+        prevented the garbage collector from collecting the object.
         '''
+
         self.routing_model = None
 
 
